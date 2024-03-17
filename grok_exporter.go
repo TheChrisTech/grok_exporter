6e008b8d66d132051ef38a5126d396c05bf18d64
TheChrisTech <chris21788@gmail.com>
2024-03-17 11:40:56 -0400

    Up Debugging level

--- grok_exporter.go
+++ grok_exporter.go
@@ -339,22 +339,18 @@ func startTailer(cfg *v3.Config, registry prometheus.Registerer) (fswatcher.File
 		err  error
 	)
 	logger := logrus.New()
-	logger.Level = logrus.WarnLevel
+	logger.Level = logrus.DebugLevel
 	switch {
 	case cfg.Input.Type == "file":
 		if cfg.Input.PollInterval == 0 {
 			tail, err = fswatcher.RunFileTailer(cfg.Input.Globs, cfg.Input.Readall, cfg.Input.FailOnMissingLogfile, logger)
 			if err != nil {
 				return nil, err
-			} else {
-				return nil, fmt.Errorf("Error: '%v'", err)
 			}
 		} else {
 			tail, err = fswatcher.RunPollingFileTailer(cfg.Input.Globs, cfg.Input.Readall, cfg.Input.FailOnMissingLogfile, cfg.Input.PollInterval, logger)
 			if err != nil {
 				return nil, err
-			} else {
-				return nil, fmt.Errorf("Error: '%v'", err)
 			}
 		}
 	case cfg.Input.Type == "stdin":
